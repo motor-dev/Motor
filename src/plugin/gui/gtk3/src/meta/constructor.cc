6c835d10eb1e30c990ec7dc1ca180da1e5a0286f
yngwe@fry <yngwe@fry>
2021-12-12 13:18:11 +0100

    Fix for older GLib/GTK version

--- src/plugin/gui/gtk3/src/meta/constructor.cc
+++ src/plugin/gui/gtk3/src/meta/constructor.cc
@@ -15,8 +15,6 @@ namespace Motor { namespace Gtk3 {
 Meta::Value Constructor::call(raw< const Meta::Method > method, Meta::Value* params, u32 nparams)
 {
     motor_forceuse(method);
-    const char** names  = static_cast< const char** >(malloca(nparams * sizeof(char*)));
-    GValue*      values = static_cast< GValue* >(malloca(nparams * sizeof(GValue)));
 
     raw< const Constructor > constructor
         = {reinterpret_cast< const Constructor* >(method.operator->())};
@@ -26,6 +24,13 @@ Meta::Value Constructor::call(raw< const Meta::Method > method, Meta::Value* par
     GParamSpec**  paramSpecs = g_object_class_list_properties(objectClass, &paramSpecCount);
 
     u32 actualParamCount = 0;
+
+#ifdef GLIB_VERSION_2_54
+    const char** names  = static_cast< const char** >(malloca(nparams * sizeof(char*)));
+    GValue*      values = static_cast< GValue* >(malloca(nparams * sizeof(GValue)));
+#else
+    GParameter* gparams = static_cast< GParameter* >(malloca(nparams * sizeof(GParameter)));
+#endif
 
     for(guint i = 0, j = 0; i < paramSpecCount; ++i)
     {
@@ -39,6 +44,7 @@ Meta::Value Constructor::call(raw< const Meta::Method > method, Meta::Value* par
            || (paramSpecs[i]->flags & G_PARAM_CONSTRUCT))
         {
             motor_assert(actualParamCount < nparams, "invalid parameter count");
+#ifdef GLIB_VERSION_2_54
             values[actualParamCount] = G_VALUE_INIT;
             if(convertMetaValueToGValue(params[j], paramSpecs[i]->value_type,
                                         &values[actualParamCount]))
@@ -46,17 +52,32 @@ Meta::Value Constructor::call(raw< const Meta::Method > method, Meta::Value* par
                 names[actualParamCount] = g_param_spec_get_name(paramSpecs[i]);
                 ++actualParamCount;
             }
+#else
+            gparams[actualParamCount].value = G_VALUE_INIT;
+            if(convertMetaValueToGValue(params[j], paramSpecs[i]->value_type,
+                                        &gparams[actualParamCount].value))
+            {
+                gparams[actualParamCount].name = g_param_spec_get_name(paramSpecs[i]);
+                ++actualParamCount;
+            }
+#endif
             ++j;
         }
     }
 
+#ifdef GLIB_VERSION_2_54
     GObjectWrapper object = {g_object_new_with_properties(type, actualParamCount, names, values)};
+#else
+    GObjectWrapper object = {(GObject*)g_object_newv(type, actualParamCount, gparams)};
+#endif
+
     motor_assert(method->overloads.count == 1,
                  "Constructor for type %s has more than one overload" | g_type_name(type));
     Meta::Type  returnType = method->overloads[0].returnType;
     Meta::Value result(returnType, &object, Meta::Value::MakeCopy);
     g_type_class_unref(objectClass);
 
+#ifdef GLIB_VERSION_2_54
     for(guint i = 0; i < actualParamCount; ++i)
     {
         g_value_unset(&values[actualParamCount - i - 1]);
@@ -64,6 +85,14 @@ Meta::Value Constructor::call(raw< const Meta::Method > method, Meta::Value* par
 
     freea(values);
     freea(names);
+#else
+    for(guint i = 0; i < actualParamCount; ++i)
+    {
+        g_value_unset(&gparams[actualParamCount - i - 1].value);
+    }
+
+    freea(gparams);
+#endif
 
     return result;
 }
