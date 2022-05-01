88faffb7d98e36d2b9e07d54218723d3503452a7
yngwe@fry <yngwe@fry>
2022-05-01 21:22:37 +0200

    Added Futex implementation for Win32
    Refactored semaphore class to leverage WaitOnAddress. It improves the
    task scheduler load test considerably (decreases frame time by 90%).

--- src/motor/3rdparty/system/win32/mak/setup.py
+++ src/motor/3rdparty/system/win32/mak/setup.py
@@ -5,7 +5,7 @@ def setup(conf):
     if 'windows' in conf.env.VALID_PLATFORMS:
         conf.start_msg_setup()
         if conf.check_lib(
-            ['kernel32', 'user32', 'advapi32', 'ole32', 'oleaut32', 'uuid', 'shell32'],
+            ['kernel32', 'user32', 'advapi32', 'ole32', 'oleaut32', 'uuid', 'shell32', 'synchronization'],
             includepath=[os.path.join(conf.path.parent.abspath(), 'api.windows')]
         ):
             conf.end_msg('from system')
