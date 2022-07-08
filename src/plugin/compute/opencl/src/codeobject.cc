775c891aecfcfe8243e0986b39158006a6181453
TryCatch2022 <234057668+TryCatch2022@users.noreply.github.com>
2022-07-08 11:10:43 +0200

    OpenCL: avoid crash when program can't be loaded.

--- src/plugin/compute/opencl/src/codeobject.cc
+++ src/plugin/compute/opencl/src/codeobject.cc
@@ -2,25 +2,35 @@
    see LICENSE for detail */
 
 #include <motor/plugin.compute.opencl/stdafx.h>
+#include <motor/scheduler/kernel/ischeduler.hh>
 #include <codeobject.hh>
 #include <context.hh>
-#include <motor/scheduler/kernel/ischeduler.hh>
 
 namespace Motor { namespace KernelScheduler { namespace OpenCL {
 
 CodeObject::CodeObject(weak< const Context > context, const inamespace& name)
     : m_kernel(name, ipath("kernel"))
+    , m_program(loadProgram(context))
 {
-    const char* kernelSource = *m_kernel.getSymbol< const char* >(
-        istring(minitl::format< 128u >("s_clKernel%d") | context->getPointerSize()));
-    const u64 kernelSourceSize = *m_kernel.getSymbol< const u64 >(
-        istring(minitl::format< 128u >("s_clKernel%dSize") | context->getPointerSize()));
-    m_program = context->buildProgram(kernelSourceSize, kernelSource);
 }
 
 CodeObject::~CodeObject()
 {
-    clReleaseProgram(m_program);
+    if(m_program) clReleaseProgram(m_program);
+}
+
+cl_program CodeObject::loadProgram(weak< const Context > context)
+{
+    if(m_kernel)
+    {
+        const char* kernelSource = *m_kernel.getSymbol< const char* >(
+            istring(minitl::format< 128u >("s_clKernel%d") | context->getPointerSize()));
+        const u64 kernelSourceSize = *m_kernel.getSymbol< const u64 >(
+            istring(minitl::format< 128u >("s_clKernel%dSize") | context->getPointerSize()));
+        return context->buildProgram(kernelSourceSize, kernelSource);
+    }
+    else
+        return 0;
 }
 
 }}}  // namespace Motor::KernelScheduler::OpenCL
