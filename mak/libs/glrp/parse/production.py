a3dc7424218fa7a2b8fbf6475c21760787851fd4
yngwe@fry <yngwe@fry>
2022-08-29 23:43:17 +0200

    glrp: Parsing with merge.
    Successfully parses some C++ snippets including merging ambiguous
    sequences.

--- mak/libs/glrp/parse/production.py
+++ mak/libs/glrp/parse/production.py
@@ -33,6 +33,32 @@ class Production(Symbol):
         else:
             raise IndexError('can only assign to production[0]')
 
+    def debug_print(self, name_map, self_indent='', inner_indent=''):
+        # type: (List[str], str, str) -> None
+        if self._production:
+            print('%s%s' % (self_indent, name_map[self._id]))
+            for p in self._production[0:-1]:
+                p.debug_print(name_map, inner_indent + '\u251c ', inner_indent + '\u2502 ')
+            self._production[-1].debug_print(name_map, inner_indent + '\u2570 ', inner_indent + '  ')
+        else:
+            print('%s%s (nil)' % (self_indent, name_map[self._id]))
+
+
+class AmbiguousProduction(Symbol):
+
+    def __init__(self, productions):
+        # type: (List[Symbol]) -> None
+        production = productions[0]
+        Symbol.__init__(self, production._id, production._start_position, production._end_position)
+        self._productions = productions
+
+    def debug_print(self, name_map, self_indent='', inner_indent=''):
+        # type: (List[str], str, str) -> None
+        print('%sAmbigous[%s]' % (self_indent, name_map[self._id]))
+        for p in self._productions[0:-1]:
+            p.debug_print(name_map, inner_indent + '\u255f ', inner_indent + '\u2551 ')
+        self._productions[-1].debug_print(name_map, inner_indent + '\u2559 ', inner_indent + '  ')
+
 
 if TYPE_CHECKING:
     from motor_typing import Any, Callable, List
--- mak/libs/glrp/symbol.py
+++ mak/libs/glrp/symbol.py
@@ -2,6 +2,7 @@ from motor_typing import TYPE_CHECKING
 
 
 class Symbol:
+
     def __init__(self, id, start_position, end_position):
         # type: (int, int, int) -> None
         self._id = id
@@ -9,6 +10,11 @@ class Symbol:
         self._end_position = end_position
         self.value = None  # type: Any
 
+    def debug_print(self, name_map, self_indent='', inner_indent=''):
+        # type: (List[str], str, str) -> None
+        print('%s"%s"' % (self_indent, name_map[self._id]))
+
 
 if TYPE_CHECKING:
+    from typing import List
     from motor_typing import Any
