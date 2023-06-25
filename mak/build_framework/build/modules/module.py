8fdebcb705ace84cab4c0c5fddead73cdd4506ce
yngwe@fry <yngwe@fry>
2023-06-25 16:57:13 +0200

    Avoid adding build-specific options during project generation

--- mak/build_framework/build/modules/module.py
+++ mak/build_framework/build/modules/module.py
@@ -126,7 +126,8 @@ def module(
     else:
         source_list = []
     preprocess = build_context.get_tgen_by_name('%s.preprocess' % name)
-    extra_includes = extra_includes + [preprocess.generated_include_node]
+    if not env.PROJECTS:
+        extra_includes = extra_includes + [preprocess.generated_include_node]
 
     module_path = None
     if path:
