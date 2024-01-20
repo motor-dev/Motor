7b5316fac18b55b37a5e3269ec60d202463c1044
yngwe@fry <yngwe@fry>
2024-01-21 00:26:30 +0100

    ircc removal (continued)

--- mak/lib/waftools/ir_compiler.py
+++ mak/lib/waftools/ir_compiler.py
@@ -14,22 +14,17 @@ class ircc(waflib.Task.Task):
             [
                 sys.executable, self.generator.bld.env.KERNEL_CLT, '--tmp',
                 self.generator.bld.bldnode.abspath(), self.inputs[0].abspath(), self.outputs[0].abspath(),
-                getattr(self, 'ircc_target').abspath()
+                'none'
             ]
         )
 
     def scan(self) -> Tuple[List[waflib.Node.Node], List[str]]:  # type: ignore
-        return self.ircc_dep_nodes + getattr(self, 'ircc_target').ant_glob('**/*.py'), []
+        return self.ircc_dep_nodes, []
 
 
 def build(build_context: build_framework.BuildContext) -> None:
     ircc.ircc_dep_nodes = []
     mak_node = build_context.motornode.make_node('mak')
     ircc.ircc_dep_nodes = [mak_node.make_node('bin/ir_compile.py')]
-    ircc.ircc_dep_nodes += mak_node.make_node('lib/ircc').ant_glob('**/*.py', excl='')
-    ircc.ircc_dep_nodes += mak_node.make_node('vendor/ply').ant_glob('**/*.py', excl='')
     if not build_context.env.PROJECTS:
-        import ircc as ircc_module
-        tmp_node = build_context.bldnode
-        ircc_module.ir_parser.IrParser(tmp_node.abspath())
         build_context.env.KERNEL_CLT = build_context.motornode.make_node('mak/bin/ir_compile.py').abspath()
