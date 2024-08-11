06910cf0caf4b4cc6a5bb9efe497a49813a7c762
yngwe <yngwe@fry>
2024-08-12 00:38:42 +0200

    fixed type annotation

--- mak/framework/configure/target/linux.lua
+++ mak/framework/configure/target/linux.lua
@@ -1,15 +1,15 @@
 ---@type Context
 context = ...
 
-for _, --[[---@type std__Packed<string>]] tool_table in ipairs(context.compilers) do
+for _, tool_table in ipairs(--[[---@type std__Packed<string>[] ]] context.compilers) do
     local tool, compiler, architecture, target, version, includes, defines = table.unpack(tool_table)
     if target:find('%-linux%-gnu') then
         context:with(context:derive(context.env), function()
             local target_name = 'linux-' .. architecture .. '-' .. tool .. '-' .. version[1] .. '.' .. version[2] .. '.' .. version[3]
             context.env.TARGET = target_name
             local setup = context:declare_command('setup:' .. target_name, 'setup', context.env)
-            ---@type string[]
-            local flavors = context.settings.flavors
+
+            local flavors = --[[---@type string[] ]] context.settings.flavors
             for _, flavor in ipairs(flavors) do
                 context:chain_command(setup, 'build:' .. target_name .. ':' .. flavor, 'build')
             end
