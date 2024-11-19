a735eed30468db34d7197ac10a240933e19e5046
TryCatch2022 <234057668+TryCatch2022@users.noreply.github.com>
2024-11-19 17:34:39 +0100

    fixes for compilation errors:
    - fixed Android include paths

--- extra/android/src/motor/filesystem/src/platform{android}/diskfolder.cc
+++ extra/android/src/motor/filesystem/src/platform{android}/diskfolder.cc
@@ -2,5 +2,5 @@
    see LICENSE for detail */
 
 // NOLINTBEGIN(bugprone-suspicious-include)
-#include "../src/platform=posix+darwin/diskfolder.cc"
+#include "../src/platform{posix+darwin}/diskfolder.cc"
 // NOLINTEND(bugprone-suspicious-include)
--- extra/android/src/motor/filesystem/src/platform{android}/file.cc
+++ extra/android/src/motor/filesystem/src/platform{android}/file.cc
@@ -2,5 +2,5 @@
    see LICENSE for detail */
 
 // NOLINTBEGIN(bugprone-suspicious-include)
-#include "../src/platform=posix+darwin/file.cc"
+#include "../src/platform{posix+darwin}/file.cc"
 // NOLINTEND(bugprone-suspicious-include)
