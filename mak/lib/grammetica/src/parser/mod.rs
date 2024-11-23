ff1cd6bcd75842bb0a0d1c61fdd7fce4500b9487
yngwe@labarbara <yngwe@labarbara>
2024-11-24 00:05:39 +0100

    [grammetica]
    - started parsing of the grammar

--- mak/lib/grammetica/src/parser/mod.rs
+++ mak/lib/grammetica/src/parser/mod.rs
@@ -1,41 +1,61 @@
-use proc_macro::{Delimiter, Group, Ident, TokenStream, TokenTree};
-use std::option::IntoIter;
 use crate::error::{Error, Result};
+use proc_macro::{Delimiter, Group, Ident, Punct, Spacing, TokenStream, TokenTree};
 
+mod rules;
 mod start;
 mod tokens;
-mod rules;
-mod params;
 mod variants;
 
-pub(crate) struct Grammar {}
+pub(crate) struct Grammar {
+    pub(crate) parameters: TokenTree,
+}
 
 fn invalid_token(token: TokenTree, expected: &str) -> Error {
     match token {
-        TokenTree::Ident(ident) =>
-            Error::new(ident.span(), format!("Unexpected identifier `{}`. {}.", ident, expected)),
-        TokenTree::Punct(punct) =>
-            Error::new(punct.span(), format!("Unexpected token `{}`. {}.", punct, expected)),
-        TokenTree::Literal(literal) =>
-            Error::new(literal.span(), format!("Unexpected literal `{}`. {}.", literal, expected)),
-        TokenTree::Group(group) =>
-            Error::new(group.span_open(), format!("Unexpected {} block. {}.", match group.delimiter() {
-                Delimiter::Brace => "{}",
-                Delimiter::Parenthesis => "()",
-                Delimiter::Bracket => "[]",
-                Delimiter::None => "macro",
-            }, expected)),
+        TokenTree::Ident(ident) => Error::new(
+            ident.span(),
+            format!("Unexpected identifier `{}`. {}.", ident, expected),
+        ),
+        TokenTree::Punct(punct) => Error::new(
+            punct.span(),
+            format!("Unexpected token `{}`. {}.", punct, expected),
+        ),
+        TokenTree::Literal(literal) => Error::new(
+            literal.span(),
+            format!("Unexpected literal `{}`. {}.", literal, expected),
+        ),
+        TokenTree::Group(group) => Error::new(
+            group.span_open(),
+            format!(
+                "Unexpected {} block. {}.",
+                match group.delimiter() {
+                    Delimiter::Brace => "{}",
+                    Delimiter::Parenthesis => "()",
+                    Delimiter::Bracket => "[]",
+                    Delimiter::None => "macro",
+                },
+                expected
+            ),
+        ),
     }
 }
 
 fn duplicate_section(token: &Ident, section: &str) -> Error {
-    Error::new(token.span(), format!("Section `{}`already defined.", section))
+    Error::new(
+        token.span(),
+        format!("Section `{}`already defined.", section),
+    )
 }
 
 fn missing_value(token: &Ident, section: &str) -> Error {
-    Error::new(token.span(), format!("Unexpected end of input: expected a value for section `{}`.", section))
+    Error::new(
+        token.span(),
+        format!(
+            "Unexpected end of input: expected a value for section `{}`.",
+            section
+        ),
+    )
 }
-
 
 fn consume_parameter(iterator: &mut proc_macro::token_stream::IntoIter) -> Option<TokenStream> {
     let value = iterator.next()?;
@@ -49,8 +69,8 @@ fn consume_parameter(iterator: &mut proc_macro::token_stream::IntoIter) -> Optio
     result.extend([value]);
     for value in iterator.by_ref() {
         if let TokenTree::Punct(punct) = &value {
-            if punct.as_char() == ':' {
-                break
+            if punct.as_char() == ';' {
+                break;
             }
         }
         result.extend([value]);
@@ -65,88 +85,91 @@ impl Grammar {
         let mut rules = None;
         let mut start = None;
         let mut variants = None;
-        let mut params = None;
+        let mut parameters: Option<TokenStream> = None;
         while let Some(section) = current.next() {
             match &section {
-                TokenTree::Ident(section_name) => {
-                    match section_name.to_string().as_str() {
-                        "tokens" => {
-                            if tokens.is_some() {
-                                return Err(duplicate_section(section_name, "tokens"));
+                TokenTree::Ident(section_name) => match section_name.to_string().as_str() {
+                    "tokens" => {
+                        if tokens.is_some() {
+                            return Err(duplicate_section(section_name, "tokens"));
+                        }
+                        match consume_parameter(&mut current) {
+                            None => {
+                                return Err(missing_value(section_name, "tokens"));
                             }
-                            match consume_parameter(&mut current) {
-                                None => {
-                                    return Err(missing_value(section_name, "tokens"));
-                                },
-                                Some(tree) => {
-                                    tokens = Some(tokens::from_dsl(tree)?);
-                                }
-                                
+                            Some(tree) => {
+                                tokens = Some(tokens::from_dsl(tree)?);
                             }
                         }
-                        "rules" => {
-                            if rules.is_some() {
-                                return Err(duplicate_section(section_name, "rules"));
-                            }
-                            match consume_parameter(&mut current) {
-                                None => {
-                                    return Err(missing_value(section_name, "rules"));
-                                },
-                                Some(tree) => {
-                                    rules = Some(rules::from_dsl(tree)?);
-                                }
-
-                            }
-                        }
-                        "start" => {
-                            if start.is_some() {
-                                return Err(duplicate_section(section_name, "start"));
-                            }
-                            match consume_parameter(&mut current) {
-                                None => {
-                                    return Err(missing_value(section_name, "start"));
-                                },
-                                Some(tree) => {
-                                    start = Some(start::from_dsl(tree)?);
-                                }
-
-                            }
-                        }
-                        "variants" => {
-                            if variants.is_some() {
-                                return Err(duplicate_section(section_name, "variants"));
-                            }
-                            match consume_parameter(&mut current) {
-                                None => {
-                                    return Err(missing_value(section_name, "variants"));
-                                },
-                                Some(tree) => {
-                                    variants = Some(variants::from_dsl(tree)?);
-                                }
-
-                            }
-                        }
-                        "params" => {
-                            if params.is_some() {
-                                return Err(duplicate_section(section_name, "params"));
-                            }
-                            match consume_parameter(&mut current) {
-                                None => {
-                                    return Err(missing_value(section_name, "params"));
-                                },
-                                Some(tree) => {
-                                    params = Some(params::from_dsl(tree)?);
-                                }
-
-                            }
-                        }
-                        _ => return Err(invalid_token(section, "Expecting a section `variants`, `tokens`, `rules`, `start`, `params`"))
                     }
+                    "rules" => {
+                        if rules.is_some() {
+                            return Err(duplicate_section(section_name, "rules"));
+                        }
+                        match consume_parameter(&mut current) {
+                            None => {
+                                return Err(missing_value(section_name, "rules"));
+                            }
+                            Some(tree) => {
+                                rules = Some(rules::from_dsl(tree)?);
+                            }
+                        }
+                    }
+                    "start" => {
+                        if start.is_some() {
+                            return Err(duplicate_section(section_name, "start"));
+                        }
+                        match consume_parameter(&mut current) {
+                            None => {
+                                return Err(missing_value(section_name, "start"));
+                            }
+                            Some(tree) => {
+                                start = Some(start::from_dsl(tree)?);
+                            }
+                        }
+                    }
+                    "variant" => match consume_parameter(&mut current) {
+                        None => {
+                            return Err(missing_value(section_name, "variant"));
+                        }
+                        Some(tree) => {
+                            variants = Some(variants::from_dsl(tree)?);
+                        }
+                    },
+                    "param" => match consume_parameter(&mut current) {
+                        None => {
+                            return Err(missing_value(section_name, "param"));
+                        }
+                        Some(tree) => {
+                            if let Some(param_list) = &mut parameters {
+                                param_list
+                                    .extend([TokenTree::Punct(Punct::new(',', Spacing::Alone))]);
+                                param_list.extend(tree);
+                            } else {
+                                parameters = Some(tree);
+                            }
+                        }
+                    },
+                    _ => {
+                        return Err(invalid_token(
+                            section,
+                            "Expecting a section `variant`, `tokens`, `rules`, `start`, `param`",
+                        ))
+                    }
+                },
+                _ => {
+                    return Err(invalid_token(
+                        section,
+                        "Expecting a section `variant`, `tokens`, `rules`, `start`, `param`",
+                    ))
                 }
-                _ => return Err(invalid_token(section, "Expecting a section `variants`, `tokens`, `rules`, `start`, `params`"))
             }
         }
-        Ok(Grammar {})
+        Ok(Grammar {
+            parameters: TokenTree::Group(Group::new(
+                Delimiter::Parenthesis,
+                parameters.unwrap_or(TokenStream::new()),
+            )),
+        })
     }
 }
-
--- mak/lib/grammetica/src/parser/params.rs
+++
@@ -1,6 +0,0 @@
-use proc_macro::{TokenStream};
-use super::Result;
-
-pub(super) fn from_dsl(grammar: TokenStream) -> Result<()> {
-    Ok(())
-}
