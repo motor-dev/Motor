99074ff97c93ea06c515eadb8736953eb445946a
TryCatch2022 <234057668+TryCatch2022@users.noreply.github.com>
2024-12-09 23:52:34 +0100

    [boltbuild]
    implemented STLIB. Switched zlib/minizip to STLIB for static builds.

--- mak/bin/boltbuild/tools/internal/link.lua
+++ mak/bin/boltbuild/tools/internal/link.lua
@@ -3,4 +3,4 @@ local context = ...
 
 context:command_driver('shlib', 'yellow', '${LINK} ${LINKFLAGS} ${LINKFLAGS_shlib} ${LINK_SRC_F:SRC} ${LINK_TGT_F:TGT[0]} ${LINK_LIBPATH_F:LIBPATHS} ${LINK_LIB_F:LIBS}')
 context:command_driver('program', 'yellow', '${LINK} ${LINKFLAGS} ${LINKFLAGS_program} ${LINK_SRC_F:SRC} ${LIB_TGT_F:TGT[0]}')
-context:command_driver('stlib', 'yellow', '${LIB} ${LIBFLAGS} ${LINK_SRC_F:SRC} ${LINK_TGT_F:TGT[0]} ${LINK_LIBPATH_F:LIBPATHS} ${LINK_LIBS_F:LIBS}')
+context:command_driver('stlib', 'yellow', '${LIB} ${LIBFLAGS} ${LIB_TGT_F:TGT[0]} ${LIB_SRC_F:SRC}')
--- src/motor/3rdparty/system/zlib/mak/build.lua
+++ src/motor/3rdparty/system/zlib/mak/build.lua
@@ -7,7 +7,21 @@ local zlib
 if context.env.check_zlib then
     zlib = Bolt.Module.pkg_config('motor.3rdparty.system.zlib', 'zlib')
 elseif context.env.ZLIB_SRC_NODE then
-    zlib = Bolt.Module.shared_library('motor.3rdparty.system.zlib', {
+    local group = context.fs_name
+    local lib_type = 'stlib'
+    if context.settings.static then
+        lib_type = 'stlib'
+        group = group .. '.static'
+    else
+        group = group .. '.default'
+    end
+    if context.settings.nobulk then
+        group = group .. '.nobulk'
+    end
+
+    zlib = Bolt.Module.module('motor.3rdparty.system.zlib', {
+        group = group,
+        features = { lib_type },
         source_patterns = {
             {path = context.env.ZLIB_SRC_NODE, pattern = '*.c'},
         },
@@ -27,15 +41,29 @@ end
 if context.env.check_minizip then
     Bolt.Module.pkg_config('motor.3rdparty.system.minizip', 'minizip')
 elseif context.env.MINIZIP_SRC_NODE then
-    Bolt.Module.shared_library('motor.3rdparty.system.minizip', {
+    local group = context.fs_name
+    local lib_type = 'stlib'
+    if context.settings.static then
+        lib_type = 'stlib'
+        group = group .. '.static'
+    else
+        group = group .. '.default'
+    end
+    if context.settings.nobulk then
+        group = group .. '.nobulk'
+    end
+
+    Bolt.Module.module('motor.3rdparty.system.minizip', {
+        group = group,
+        features = { lib_type },
         source = {
-            { base_path = context.env.ZLIB_SRC_NODE:make_node('contrib/minizip'),
-              full_path = context.env.ZLIB_SRC_NODE:make_node('contrib/minizip/ioapi.c') },
-            { base_path = context.env.ZLIB_SRC_NODE:make_node('contrib/minizip'),
-              full_path = context.env.ZLIB_SRC_NODE:make_node('contrib/minizip/unzip.c') },
+            { base_path = context.env.MINIZIP_SRC_NODE:make_node('contrib/minizip'),
+              full_path = context.env.MINIZIP_SRC_NODE:make_node('contrib/minizip/ioapi.c') },
+            { base_path = context.env.MINIZIP_SRC_NODE:make_node('contrib/minizip'),
+              full_path = context.env.MINIZIP_SRC_NODE:make_node('contrib/minizip/unzip.c') },
         },
         public_includes = {
-            context.env.ZLIB_SRC_NODE:make_node('contrib/minizip'),
+            context.env.MINIZIP_SRC_NODE:make_node('contrib/minizip'),
         },
         public_defines = {
         },
